9f0e1202e9049626621b16f781e78ffc4859a28c
Hope Bradley <hope.e.bradley@gmail.com>
2021-07-04 18:06:42 -0400

    add create and show actions to userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
 
     def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
     def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user, status: :created
+        else
+            render json: { error: "Unauthorized" }, status: :unauthorized
+        end
     end
 
     private
@@ -11,4 +24,5 @@ class UsersController < ApplicationController
     def user_params
         params.permit(:username, :name, :activity_level, :password, :password_confirmation)
     end
+
 end
