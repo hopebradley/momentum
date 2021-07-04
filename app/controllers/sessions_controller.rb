c04feb84084b398c0dfd5170a83daca4d814fd1d
Hope Bradley <hope.e.bradley@gmail.com>
2021-07-04 18:14:26 -0400

    add auth routes and sessionscontroller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,22 @@
 class SessionsController < ApplicationController
 
     def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { errors: ["Unauthorized"]}, status: :unauthorized
+        end
     end
 
     def destroy
+        if session[:user_id]
+            delete :user_id
+            head :no_content
+        else
+            render json: { errors: ["Unauthorized"]}, status: :unauthorized
+        end
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:show, :create]
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  get '/me', to: 'users#show'
+  delete '/logout', to: 'sessions#destroy'
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
