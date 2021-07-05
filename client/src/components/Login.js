b394699acea8548596306145c0de210ed8996edf
Hope Bradley <hope.e.bradley@gmail.com>
2021-07-05 07:58:14 -0400

    work on login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,36 @@
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm';
+import React, { useState } from 'react';
 
 const Login = () => {
 
+    const [hasAccount, setHasAccount] = useState(true);
+
+    function showLogin() {
+        if (hasAccount) {
+            return (
+                <div className="login">
+                    <LoginForm />
+                    <h3>OR</h3>
+                    <button onClick={() => setHasAccount(false)}>Sign Up</button>
+                </div>     
+            )
+            
+        }
+        else {
+            return (
+                <div className="login">
+                    <SignUpForm />
+                    <h3>Have an account?</h3>
+                    <button onClick={() => setHasAccount(true)}>Login</button>
+                </div> 
+            )
+        }
+    }
+
     return (
         <div>
-            <LoginForm/>
-            <SignUpForm/>
+            {showLogin()}
         </div>
     )
 }
