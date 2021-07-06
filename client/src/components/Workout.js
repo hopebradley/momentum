3019ab2f5861a4a2625eb27074009cbe596eb446
Hope Bradley <hope.e.bradley@gmail.com>
2021-07-05 20:02:48 -0400

    add styles

--- client/src/components/Workout.js
+++ client/src/components/Workout.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 const Workout = ({workout}) => {
 
+    function minutesToHours() {
+        const totalMinutes = workout.minutes;
+        if (totalMinutes < 59) {
+            return "00:"+ totalMinutes;
+        }
+        else {
+            const extraMins = totalMinutes%60;
+            const hours = (totalMinutes-extraMins)/60;
+            return hours+":"+extraMins;
+        }
+    }
+
     return (
         <div className="workout">
-            <h1>I am a workout</h1>
-            {workout.title}
+            <h3 className="workout-title">{workout.title.toLowerCase()}</h3>
+            <h3>activity –> {workout.activity.toLowerCase()}</h3>
+            <h3>time taken –> {minutesToHours()}</h3>
         </div>
         
     )
